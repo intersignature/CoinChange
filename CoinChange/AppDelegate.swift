10357e6bcbceee68f44beeae0d2295e9d1e852eb
Sirichai <sirichai.b@23perspective.com>
2020-07-04 21:51:47 +0700

    [ add ] base project

---
+++ CoinChange/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CoinChange
+//
+//  Created by User23 on 4/7/2563 BE.
+//  Copyright © 2563 Sirichai. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ CoinChange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinChange
+//
+//  Created by User23 on 4/7/2563 BE.
+//  Copyright © 2563 Sirichai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
